0d9d0086fdf9aae3217ba8fa74c0669163285609
uni-paul-taylor2 <123190211+uni-paul-taylor2@users.noreply.github.com>
2023-04-30 22:25:21 +0000

    2018 semester 3 number 5 logic update

--- semester 3/2018/q5.cpp
+++ semester 3/2018/q5.cpp
@@ -13,15 +13,15 @@ using namespace std;
 int main(){
 	srand(time(NULL));
 	float sum=0;
-	int cards[52], i, card;
+	int cards[52], i, card, roll;
 	for(i=0;i<52;i++)
 		cards[i]=1+(i%13); //1 to 13 that will happen 4 times
 	
 	for(i=0;i<200;i++){ //the simulation now
 		sum-=20; //the cost to bet
+		roll=1+(rand()%6); //dice roll from 1 to 6
+		if(roll%2==1) continue; //bet lost first condition
 		card=cards[ rand()%52 ]; //draw a card
-		if(card%2==1) continue; //bet lost first condition
-		card=cards[ rand()%52 ]; //draw another card
 		if(card%2==0||card==11||card==13) continue; //bet lost second condition
 		sum+=20+(3*card); //only bet win condition
 	}
